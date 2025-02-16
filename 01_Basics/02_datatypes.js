6226d8e7ec915b4e1bd2a7215982e1691e8e5e72
ShrutiChaudhari24 <147521240+ShrutiChaudhari24@users.noreply.github.com>
2025-02-16 08:45:09 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello")
+// alert( 3 + 3 )      we are using nodejs , not browser
+
+console.log(10 + 10) ; console.log("Shruti")  // This is not a good practice code Readability should be high 
+
+ console.log(10 
+     + 
+    10) 
+// This is not a good practice code Readability should be high 
+
+
+let name = "Shruti"  // String
+let age = 20  //number 
+let isLoggedIn = true  //boolean
+let state ;
+
+//number  => 2 to power 53
+//bigint
+//string  =>""
+//boolean  => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof "Shruti")
+console.log(typeof age)
+console.log(typeof null);    //object
+console.log(typeof undefined); //undefined
